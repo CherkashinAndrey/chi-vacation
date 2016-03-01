584193d3e3273fd2216cd4f0d5ae05e2a2b0acc4
Andrey Cherkashin <tosmsandreyka@gmail.com>
2016-03-01 16:22:39 +0200

    add admin vv

--- src/app/pages/admin/vv.controller.js
+++ src/app/pages/admin/vv.controller.js
@@ -1,50 +1,54 @@
 export default class VvController {
-  constructor (firebaseService, $scope) {
+  constructor (firebaseService) {
     'ngInject';
 
     this.oneThing = [];
     this.awesomeThings = [];
     this.userName = [];
-    var temp = [];
-    this.classAnimation = '';
-    this.activate(firebaseService);
-    this.calendarView = 'month';
-    this.calendarDay = new Date();
-    this.$scope =  $scope;
-
-
-    // $scope.$watch('this.oneThing.name', function(){
-    // console.log('changed', this.oneThing );
-    // });
-
-    // scope.obj.vacations.list[0].startDate
-    // scope.obj.vacations.list[0].endDate
-
     this.events = [];
-// endsAt: new Date(2016,8,10,15)
+    this.newEvent = {};
+    this.selected = "sds";
+    this.classAnimation = '';
+    this.calendarView = 'month';
+    this.activate(firebaseService);
+    this.calendarDay = new Date();
+    this.newEvent.startsAt = new Date(); 
+    this.newEvent.endsAt = new Date();
   }
 
+  setDateInfo() {
+    var _this = this;
 
-// button test 
-  showEvents1() {
-    console.log("EVENTS1");
-     //debugger
-    this.events = [
+    _this.events = [];
+    angular.forEach(this.awesomeThings, function(value, key){
+
+      if (value.vacations === undefined) {
+        return
+      }else 
       {
-        title: 'My event title', // The title of the event 
-        type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special 
-        startsAt: new Date('Thu Feb 25 2016 21:11:58 GMT+0200 (Финляндия (зима))'), // A javascript date object for when the event starts 
-        endsAt: new Date('Thu Feb 26 2016 21:11:58 GMT+0200 (Финляндия (зима))'), // Optional - a javascript date object for when the event ends 
-        editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable. If set to false will also prevent the event from being dragged and dropped. 
-        deletable: false, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable 
-        incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view 
-        recursOn: 'year' // If set the event will recur on the given period. Valid values are year or month 
-      }]
-  }
 
+        if(value.vacations.list[0].startDate >= _this.newEvent.startsAt &&
+         value.vacations.list[0].startDate <= _this.newEvent.endsAt) {
+
+          let events = 
+        {
+              title: value.firstName, // The title of the event 
+              type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special 
+              startsAt: new Date(value.vacations.list[0].startDate), // A javascript date object for when the event starts 
+              endsAt: new Date(value.vacations.list[0].endDate), // Optional - a javascript date object for when the event ends 
+              editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable. If set to false will also prevent the event from being dragged and dropped. 
+              deletable: false, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable 
+              incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view 
+              recursOn: 'year' // If set the event will recur on the given period. Valid values are year or month 
+            }
+            _this.events.push(events);
+          }
+        }
+      });
+  }
   activate(firebaseService) {
     var _this = this;
-    firebaseService.getUsersList().then( d => {debugger;
+    firebaseService.getUsersList().then( d => {
       _this.awesomeThings = d;
     });
   }
